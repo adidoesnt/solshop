b3aee08b9f27e70185ded93ab250325df55604da
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-03-06 19:12:05 +0000

    update navigation

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -5,7 +5,7 @@
 <header class="flex h-16 items-center bg-gray-800 px-6 text-white shadow-md">
 	<div class="flex-1">
 		<h1 class="text-xl font-semibold capitalize">
-			{page.url.pathname.split('/')[1] || 'Dashboard'}
+			{page.url.pathname.split('/')[1]}
 		</h1>
 	</div>
 </header>
